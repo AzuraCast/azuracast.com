8338e84488c71a66b5d343c437812e6b5eaa0a46
Buster "Silver Eagle" Neece <loobalightdark@gmail.com>
2018-12-31 06:38:50 -0600

    Update logo, icons and Swagger UI.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,12 +5,12 @@ module.exports = {
     ga: 'UA-120542341-1',
     head: [
         ['link', { rel: 'icon', href: `/img/logo.png` }],
-        ['meta', { name: 'theme-color', content: '#2196F3' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-icon-152x152.png` }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/ms-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#2196F3' }]
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: `/icons/apple-touch-icon.png` }],
+        ['link', { rel: 'icon', sizes: '32x32', href: `/icons/favicon-32x32.png` }],
+        ['link', { rel: 'icon', sizes: '16x16', href: `/icons/favicon-16x16.png` }],
+        ['link', { rel: 'shortcut icon', href: `/icons/favicon.ico` }],
+        ['meta', { name: 'msapplication-TileColor', content: '#2196F3' }],
+        ['meta', { name: 'theme-color', content: '#2196F3' }]
       ],
     themeConfig: {
         repo: 'azuracast/azuracast',
