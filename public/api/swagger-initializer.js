2cfdc40f3a97a4f5253b04355a3f7e103bc1bd26
Buster Neece <buster@busterneece.com>
2023-10-10 21:08:38 -0500

    Update default API URL.

--- public/api/swagger-initializer.js
+++ public/api/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/AzuraCast/AzuraCast/master/web/static/api/openapi.yml",
+    url: "https://raw.githubusercontent.com/AzuraCast/AzuraCast/main/web/static/openapi.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
