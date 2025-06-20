59aecf9e4a9625914c5caeff43e8adaf958f561c
Buster Neece <buster@busterneece.com>
2025-06-20 14:59:51 -0500

    Update Astro and other deps.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,60 +3,70 @@ import { defineConfig } from 'astro/config';
 import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
+
 export default defineConfig({
   server: {
-    host: true
+    host: true,
   },
-  site: 'https://www.azuracast.com',
+  site: "https://www.azuracast.com",
   vite: {
     ssr: {
-      noExternal: ['bootstrap', 'roboto-font']
-    }
+      noExternal: ["bootstrap", "roboto-font"],
+    },
   },
   integrations: [
     starlight({
-      title: 'AzuraCast',
+      title: "AzuraCast",
       logo: {
-        src: './src/images/logo.svg'
+        src: "./src/images/logo.svg",
       },
-      favicon: '/img/logo.svg',
-      customCss: [
-        './src/scss/starlight.scss'
+      favicon: "/img/logo.svg",
+      customCss: ["./src/scss/starlight.scss"],
+      social: [
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/azuracast",
+        },
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/azuracast/azuracast",
+        },
       ],
-      social: {
-        discord: 'https://discord.gg/azuracast',
-        github: 'https://github.com/azuracast/azuracast',
-      },
       editLink: {
-        baseUrl: 'https://github.com/azuracast/azuracast.com/tree/main',
+        baseUrl: "https://github.com/azuracast/azuracast.com/tree/main",
       },
       sidebar: [
-        { label: 'Home', link: '/' },
-        { label: 'About AzuraCast', link: '/docs' },
-        { label: 'Live Demo', link: '/docs/live-demo'},
-        { label: 'Install AzuraCast', link: '/docs/getting-started/installation' },
-        { label: 'Update AzuraCast', link: '/docs/getting-started/updates' },
+        { label: "Home", link: "/" },
+        { label: "About AzuraCast", link: "/docs" },
+        { label: "Live Demo", link: "/docs/live-demo" },
         {
-          label: 'Help',
-          autogenerate: { directory: 'docs/help' },
+          label: "Install AzuraCast",
+          link: "/docs/getting-started/installation",
+        },
+        { label: "Update AzuraCast", link: "/docs/getting-started/updates" },
+        {
+          label: "Help",
+          autogenerate: { directory: "docs/help" },
         },
         {
-          label: 'User Guide',
-          autogenerate: { directory: 'docs/user-guide' }
+          label: "User Guide",
+          autogenerate: { directory: "docs/user-guide" },
         },
         {
-          label: 'Administrator Guide',
-          autogenerate: { directory: 'docs/administration' }
+          label: "Administrator Guide",
+          autogenerate: { directory: "docs/administration" },
         },
         {
-          label: 'Contribute',
-          autogenerate: { directory: 'docs/contribute' },
+          label: "Contribute",
+          autogenerate: { directory: "docs/contribute" },
         },
         {
-          label: 'For Developers',
-          autogenerate: { directory: 'docs/developers'}
-        }
+          label: "For Developers",
+          autogenerate: { directory: "docs/developers" },
+        },
       ],
-    })
-  ]
+    }),
+  ],
 });
