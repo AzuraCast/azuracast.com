8efd94bedcd4514540ce0fa373b02ad34912c42d
Buster Neece <buster@busterneece.com>
2026-03-24 23:11:39 -0500

    Update to Astro 6.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 // src/content/config.ts
 import { defineCollection } from 'astro:content';
+import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-    docs: defineCollection({ schema: docsSchema() }),
+    docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
